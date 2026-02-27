ac9b5814106d020cb81be069c3034bdf99447441
cse-kumar-milind <cse.kumar.milind@gmail.com>
2026-02-27 20:05:24 +0530

    SpringMVC-Maven

---
+++ springmvc_maven/src/main/java/com/springmvc/webapp/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package com.springmvc.webapp.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomerController {
+	
+	@GetMapping("/customer")
+	public String getAllCustomers() {
+		return "customer";
+	}
+
+}
